8bc0e5edcc8357fd6d1aaae07b48463fad7dc7e2
choiwoojin <ggogo33@daum.net>
2024-10-12 16:16:15 +0900

    [Feat] src/services 폴더 생성 mongodb 커넥팅 파일 TypeScript 로 작성

---
+++ src/services/mongodb.ts
@@ -0,0 +1,26 @@
+import { MongoClient, ServerApiVersion } from 'mongodb';
+
+const uri: string = "mongodb+srv://cwjin94:1q2w3e4r@cluster0.hw55t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client: MongoClient = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run(): Promise<void> {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("todo-app").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
