ce06435d8f4b542a54d677a7c54d0e85a92f0b9e
choiwoojin <ggogo33@daum.net>
2024-10-12 17:44:47 +0900

    [Fix] mongoDB connect script connect/disconnect 로 분리

--- src/services/mongodb.ts
+++ src/services/mongodb.ts
@@ -2,25 +2,31 @@ import { MongoClient, ServerApiVersion } from 'mongodb';
 
 const uri: string = "mongodb+srv://cwjin94:1q2w3e4r@cluster0.hw55t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// MongoClient 생성
 const client: MongoClient = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
-async function run(): Promise<void> {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("todo-app").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+export async function connect(): Promise<void> {
+    try {
+        // 서버에 클라이언트 연결 (v4.7부터는 선택사항)
+        await client.connect();
+        // 성공적인 연결 확인을 위한 ping 전송
+        await client.db("todo-app").command({ ping: 1 });
+        console.log("MongoDB 연결 성공");
+    } catch (error) {
+        console.error("MongoDB 연결 중 오류 발생 : ", error);
+        throw error;
+    }
 }
-run().catch(console.dir);
+
+export async function disconnect(): Promise<void> {
+    await client.close();
+    console.log("MongoDB 연결 종료");
+}
+
+export { client };
