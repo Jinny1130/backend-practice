a390e4daf90cbe227d669339e1e5363e8d5e9e25
choiwoojin <ggogo33@daum.net>
2024-10-26 18:17:18 +0900

    [FIx] TodoService는 controller의 호출을 받아 service logic을 처리하도록  수정

--- src/services/TodoService.ts
+++ src/services/TodoService.ts
@@ -45,69 +45,63 @@ export async function getTodos(id?: string | null) {
     }
 }
 
-// // CRUD 중 Create
-// export async function POST(request: Request) {
-//     try {
-//         const todo = await request.json();
-//         await connect();
-//         const db = client.db(dbName);
-//         const collection = db.collection(collectionName);
+// CRUD 중 Create
+export async function addTodo(todo: object) {
+    try {
+        await connect();
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
 
-//         const result = await collection.insertOne(todo);
-//         const newTodo = await collection.findOne({ _id: result.insertedId });
-//         return NextResponse.json(transformId(newTodo), { status: 201 });
-//     } catch (error) {
-//         console.error('MongoDB 생성 중 오류 발생:', error);
-//         return NextResponse.json({ error: '서버 오류가 발생했습니다.' }, { status: 500 });
-//     } finally {
-//         await disconnect();
-//     }
-// }
+        const result = await collection.insertOne(todo);
+        const newTodo = await collection.findOne({ _id: result.insertedId });
 
-// // CRUD 중 Update
-// export async function PUT(request: Request) {
-//     try {
-//         const { id, ...updateData } = await request.json();
-//         await connect();
-//         const db = client.db(dbName);
-//         const collection = db.collection(collectionName);
+        return transformId(newTodo);
+    } catch (error) {
+        throw new Error('할일을 추가 중 오류가 발생했습니다.');
+    } finally {
+        await disconnect();
+    }
+}
 
-//         const result = await collection.findOneAndUpdate(
-//             { _id: new ObjectId(id) },
-//             { $set: updateData },
-//             { returnDocument: 'after' }
-//         );
+// CRUD 중 Update
+export async function modifyTodo(modifyTodo: any) {
+    try {
+        await connect();
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
 
-//         if (result) {
-//             return NextResponse.json(transformId(result), { status: 200 });
-//         }
-//         return NextResponse.json({ error: '할 일을 찾을 수 없습니다.' }, { status: 404 });
-//     } catch (error) {
-//         console.error('MongoDB 업데이트 중 오류 발생:', error);
-//         return NextResponse.json({ error: '서버 오류가 발생했습니다.' }, { status: 500 });
-//     } finally {
-//         await disconnect();
-//     }
-// }
+        const { id, ...updateData } = modifyTodo;
+        const updatedTodo = await collection.findOneAndUpdate(
+            { _id: new ObjectId(id) },
+            { $set: updateData },
+            { returnDocument: 'after' }
+        )
 
-// // CRUD 중 Delete
-// export async function DELETE(request: Request) {
-//     try {
-//         const { id } = await request.json();
-//         await connect();
-//         const db = client.db(dbName);
-//         const collection = db.collection(collectionName);
+        console.log(updatedTodo);
 
-//         const result = await collection.findOneAndDelete({ _id: new ObjectId(id) });
+        return transformId(updatedTodo)
 
-//         if (result) {
-//             return NextResponse.json({ message: '할 일이 삭제되었습니다.' }, { status: 200 });
-//         }
-//         return NextResponse.json({ error: '할 일을 찾을 수 없습니다.' }, { status: 404 });
-//     } catch (error) {
-//         console.error('MongoDB 삭제 중 오류 발생:', error);
-//         return NextResponse.json({ error: '서버 오류가 발생했습니다.' }, { status: 500 });
-//     } finally {
-//         await disconnect();
-//     }
-// }
+    } catch(error) {
+        throw new Error('할일을 수정 중 오류가 발생했습니다.')
+    } finally {
+        await disconnect();
+    }
+}
+
+// CRUD 중 Delete
+export async function deleteTodo(todo: any) {
+    try {
+        await connect();
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
+        
+        const result = await collection.findOneAndDelete({ _id: new ObjectId(todo.id) });
+
+        return result;
+
+    } catch (error) {
+        throw new Error('할일을 삭제 중 오류가 발생했습니다.');
+    } finally {
+        await disconnect();
+    }
+}
